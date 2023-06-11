1d02ce3fb799694a10d05cca56cbbab2d3637f6a
jennie <jennie.tang@my.jcu.edu.au>
2023-06-12 05:56:20 +1000

    Add prac 2 files

---
+++ prac_02/password_stars.py
@@ -0,0 +1,18 @@
+"""Module docstring"""
+
+
+# imports
+# CONSTANTS
+
+def main():
+    """Function docstring"""
+    # statements...
+    do_stuff()
+
+
+def do_stuff():
+    """Function docstring"""
+    # statements...
+
+
+main()

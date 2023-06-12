e89fa3958b3fbfce592513863c2166626f5d5bed
jennie <jennie.tang@my.jcu.edu.au>
2023-06-12 14:44:57 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,18 +1,33 @@
-"""Module docstring"""
+"""
+To enter appropriate password and display in stars.
+"""
 
 
 # imports
 # CONSTANTS
 
 def main():
-    """Function docstring"""
-    # statements...
-    do_stuff()
+    password = get_password()
+    print("\nYou have create a password.", sep=' ')
+    display_asterisks(password)
 
 
-def do_stuff():
-    """Function docstring"""
-    # statements...
+def display_asterisks(password):
+    print(password * '*')
+
+
+def get_password():
+    password = password_length_check("Enter password: ", 10)
+    return password
+
+
+def password_length_check(prompt, minimum_length):
+    """Gets the password and check with minimum_length and returns length of password"""
+    password = str(input(prompt))
+    while len(password) < minimum_length:
+        print("Invalid password.")
+        password = str(input(prompt))
+    return len(password)
 
 
 main()

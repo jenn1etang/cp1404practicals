86e238457d617735ee46134e142c56f17e7e709c
jennie <jennie.tang@my.jcu.edu.au>
2023-06-20 13:50:11 +1000

    Add program for selecting random number from 1 to 100 and inclusive.

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,4 +1,4 @@
-'''
+"""
 Comments for each line tests on console.
 
 import random
@@ -6,7 +6,8 @@ import random
 print(random.randint(5, 20))  # line 1
 print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
-'''
+"""
+
 
 # line 1
 # for line 1, the largest number is 20 and the smallest is 5.
@@ -19,5 +20,9 @@ print(random.uniform(2.5, 5.5))  # line 3
 # line 3
 # for line 3, the largest number I get is 5.362455591037363. However, the largest number should be 5.5 and
 # the smallest number should be 2.5 according to the code.
-# Because this randomly select a number including decimals, this requires to test a lot to get the largest and the smallest number.
+# Because this randomly select a number including decimals, this requires to test a lot to get the largest
+# and the smallest number.
 
+# print a random number from 1 to 100 and inclusive
+import random
+print(random.randint(1, 100))

2b0cff70ddbbbb0e743df0fb43f44068af5fdf99
jennie <jennie.tang@my.jcu.edu.au>
2023-06-27 15:12:00 +1000

    Change the income report into a function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -18,18 +18,29 @@ Starter code for cumulative total income program
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    # This variable stores the number of months
+    number_of_months = int(input("How many months? "))
 
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
 
+    display_monthly_income_report(incomes, number_of_months)
+
+
+def display_monthly_income_report(incomes, number_of_months):
+    """
+    This function will print a report of monthly income.
+    :param incomes:
+    :param number_of_months:
+    :return:
+    """
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f'Month {month:2} - Income: ${income:10.2f}\t\t\tTotal: ${total:10.2f}')
 
 
 main()

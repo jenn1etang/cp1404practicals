2aeceb3292b827558f236aec8f0c86626b793932
jennie <jennie.tang@my.jcu.edu.au>
2023-06-29 21:12:16 +1000

    Add display user data program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -7,13 +7,20 @@ Actual:
 
 def main():
     user_data = {}
-
     user_email = str(input("Email: "))
 
     while user_email != "":
         name = extract_name(user_email)
+        is_name = str(input(f"Is your name {name}? (Y/n) ")).upper()
+        if is_name != 'Y' and is_name != "":
+
+            print(f"Name: {name}")
         user_data[name] = user_email
         user_email = str(input("Email: "))
+
+    print()
+    for name, user_email in user_data.items():
+        print(f"{name} ({user_email})")
 
 
 def extract_name(user_email):

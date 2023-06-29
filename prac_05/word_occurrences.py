7346f66a062adfb9af0866d4fd56ee2edacf570e
jennie <jennie.tang@my.jcu.edu.au>
2023-06-29 20:23:18 +1000

    Complete word occurrences exercise

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,7 +1,7 @@
 """
 Word occurrences
 Estimate: 30minutes
-Actual:
+Actual:   39minutes
 """
 
 word_count = {}
@@ -10,5 +10,14 @@ text = str(input("Text: "))
 text = text.split()
 print(text)
 
+for word in text:
+    if word in word_count:
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
 
+max_length_word = max(text, key=len)
+
+for word, count in sorted(word_count.items()):
+    print(f"{word:{len(max_length_word)+1}}: {count}")
 

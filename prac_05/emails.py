4710304491d793384766973eb3998bd430c362ed
jennie <jennie.tang@my.jcu.edu.au>
2023-06-29 21:56:11 +1000

    Complete email exercise

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -12,15 +12,26 @@ def main():
     while user_email != "":
         name = extract_name(user_email)
         is_name = str(input(f"Is your name {name}? (Y/n) ")).upper()
-        if is_name != 'Y' and is_name != "":
-
-            print(f"Name: {name}")
+        name = name_checker(is_name, name)
         user_data[name] = user_email
         user_email = str(input("Email: "))
 
     print()
     for name, user_email in user_data.items():
         print(f"{name} ({user_email})")
+
+
+def name_checker(is_name, name):
+    if is_name != 'Y' and is_name != "":
+        name = "".join(character for character in name if not character.isdigit())
+        name = list(name)
+        name[3] = name[3].upper()
+        part1 = ''.join(name[:3])
+        part2 = ''.join(name[3:])
+        name = [part1, part2]
+        name = " ".join(name)
+        print(f"Name: {name}")
+    return name
 
 
 def extract_name(user_email):

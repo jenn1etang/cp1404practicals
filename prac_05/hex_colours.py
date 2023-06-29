4e3c0c8885c0ca4fcb4c7ef4994b299384e342a0
jennie <jennie.tang@my.jcu.edu.au>
2023-06-29 19:36:27 +1000

    Complete hex colours exercise

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,22 +1,21 @@
 """
-
+Get colour name to display colour code
 """
 
-COLOUR_NAME_TO_CODE = {"Acid Green": "#b0bf1a", "Amber": "#ffbf00", "Apricot": "#fbceb1", "Aqua": "#00ffff",
-                       "Ash Grey": "#b2beb5", "Beige": "#f5f5dc", "Black": "#000000", "Gray": "#bebebe",
-                       "Indigo": "#4b0082", "Maroon": "#b03060"}
+COLOUR_NAME_TO_CODE = {"acidgreen": "#b0bf1a", "amber": "#ffbf00", "apricot": "#fbceb1", "aqua": "#00ffff",
+                       "ashgrey": "#b2beb5", "beige": "#f5f5dc", "black": "#000000", "gray": "#bebebe",
+                       "indigo": "#4b0082", "maroon": "#b03060"}
 
 
 def main():
-    colour_name = str(input("Enter colour name: ")).lower().capitalize()
-    print(colour_name)
+    colour_name = str(input("Enter colour name: ")).lower()
 
     while colour_name != "":
         try:
             print(colour_name, "is", COLOUR_NAME_TO_CODE[colour_name])
         except KeyError:
             print("Invalid colour name")
-        colour_name = input("Enter colour name: ").lower().capitalize()
+        colour_name = input("Enter colour name: ").lower()
 
 
 main()

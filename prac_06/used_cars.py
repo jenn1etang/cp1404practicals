2186ca4c49c9cc0ea1d10bb52936936d0b9c65d0
jennie <jennie.tang@my.jcu.edu.au>
2023-07-24 15:26:03 +1000

    Add complete car.py and used_cars.py

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -2,6 +2,8 @@
 CP1404/CP5632 Practical - Client code to use the Car class.
 Note that the import has a folder (module) in it.
 This is why we name our folders with no spaces or capitals, as valid module names.
+Estimate time: 30mins
+Actual time: 1hour
 """
 
 from prac_06.car import Car
@@ -13,6 +15,12 @@ def main():
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
     print(my_car)
+
+    limo = Car('limo', 100)
+    limo.add_fuel(20)
+    print(f"Car has fuel: {limo.fuel}")
+    limo.drive(115)
+    print(limo)
 
 
 main()

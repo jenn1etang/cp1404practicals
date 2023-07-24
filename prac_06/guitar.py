1d5505f9816d8f42d07151e62303568b715d8c97
jennie <jennie.tang@my.jcu.edu.au>
2023-07-24 16:10:35 +1000

    Add Guitar class

---
+++ prac_06/guitar.py
@@ -0,0 +1,14 @@
+"""
+CP1404 -
+Estimate time:
+Actual time:
+"""
+
+
+class Guitar:
+    def __init__(self, name="", _year=0, _cost=0):
+        self. name = name
+        self.year = _year
+        self.cost = _cost
+
+    def

37019ca4e3c76aa0e205a12e64a9d07b7bd949d4
jennie <jennie.tang@my.jcu.edu.au>
2023-07-24 15:50:28 +1000

    Add str method for ProgrammingLanguage class

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+"""
+CP1404 - Language program
+Estimate time: 30mins
+Actual time:
+"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+
+main()
--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -5,7 +5,8 @@ CP1404 - programming language class
 
 class ProgrammingLanguage:
 
-    def __init__(self, typing, reflection, _year):
+    def __init__(self, language, typing, reflection, _year):
+        self.language = language
         self.typing = typing
         self.reflection = reflection
         self._year = _year
@@ -15,3 +16,6 @@ class ProgrammingLanguage:
             return True
         else:
             return False
+
+    def __str__(self):
+        return f"{self.language}, {self.typing} Typing, Reflection={self.is_dynamic()}, First appeared in {self._year}"

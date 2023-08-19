826dc6318ffecca44624dfd4e68a21af42f449c7
jennie <jennie.tang@my.jcu.edu.au>
2023-08-20 06:31:34 +1000

    Complete dynamic_labels files

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,29 @@
+"""
+Estimate time: 2hours
+Actual time: 1hour
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Olivia", "John", "Mandy", "David", "Jennie"]
+
+    def build(self):
+        """..."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.names:
+            label = Label(text = name)
+            self.root.ids.main.add_widget(label)
+
+
+DynamicLabelsApp().run()
